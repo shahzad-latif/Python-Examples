8b1d6a77bc1cc5a4720688678f42dd6450aed542
Shahzad Latif <softscientist@gmail.com>
2021-10-13 18:20:02 +0500

    Regular expression examples

---
+++ regex/regex.py
@@ -0,0 +1,36 @@
+import re
+
+data = "My 2 favorite numbers are 14 and 30"
+
+#get all numbers having one or more digits
+result = re.findall('[0-9]+', data)
+
+print(result)
+
+#find all upper case vowels
+result = re.findall('[AEIOU]+', data)
+
+print(result)
+
+#find all lower case vowels
+result = re.findall('[aeiou]+', data)
+
+print(result)
+
+#find all words starging with f having any number of 
+#letters and ending with a space till the last space!!
+# .+ greedy search!
+result = re.findall('f.+\s', data)
+
+print(result)
+
+# ? makes it not greedy! so first occurance would be enough
+result = re.findall('f.+?\s', data)
+
+print(result)
+
+#get @ with before and after any non space characters
+data = "From username@domain.com at 3:00 PM"
+result = re.findall('\S+@\S+', data)
+
+print(result)
